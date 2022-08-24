587da25447e5f2c318600076e961b1397175b330
Your Name <tpunchihewage@gmail.com>
2022-08-24 15:34:27 +0200

    added js files

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 class Employee {
-  constructor(firstname, lastname) {
-    this.firstname = firstname;
-    this.lastname = lastname;
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
     // Complete the code!
   }
   fullName(fName, lName) {
-    this.firstname = fName;
-    this.lastname = lName;
+    this.firstName = fName;
+    this.lastName = lName;
 
     console.log(`${fName} ${lName}`);
   }
   email(fName, lName) {
-    this.firstname = fName;
-    this.lastname = lName;
+    this.firstName = fName;
+    this.lastName = lName;
 
     console.log(`${fName}.${lName}@company.com`);
   }
